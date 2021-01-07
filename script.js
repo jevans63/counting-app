885f8d1f47ae61a667965289dc4ea31ad4fdc906
jevans63 <jevans63@kent.edu>
2021-01-07 10:48:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,8 @@ var num = 0;
 function displayUpdate(value){
     num += parseFloat(value);
     display.innerHTML = num;
+    if(num == 15){
+        display.innerHTML = "GOAL";
+    }
 }
 ////////////////////////////////////////////////////////////////
