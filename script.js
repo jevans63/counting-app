0d0c68d7a49169b8694aa83fa9c6fd5ba8f581e6
jevans63 <jevans63@kent.edu>
2021-01-09 12:23:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ var num = 0;
 
 //
 function displayUpdate(value){
-    num += parseFloat(value);
-    display.innerHTML = num;
+    if(num < 15){
+        num += parseFloat(value);
+        display.innerHTML = num;
+    }
+
     if(num == 15){
         display.innerHTML = "GOAL";
         document.getElementById("display1").style.backgroundColor = "lightgreen";
